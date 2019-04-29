6f16ff6f43ad157906e8c0a1d9bda9364ded07bf
WTBacon <wtbacon514@gmail.com>
2019-04-29 16:42:11 +0900

    add some comments and some test cases

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -3,18 +3,27 @@ package lexer
 import "github.com/WTBacon/goInterpreter/token"
 
 type Lexer struct {
-	input        string
-	position     int  // 常に最後に読んだ位置を示す
-	readPosition int  // これから読み込む位置を示す
-	ch           byte // 現在検査中の文字
+	input        string // ソースコード
+	position     int    // 常に最後に読んだ位置を示す（chの位置を示すインデクス）
+	readPosition int    // 次に読み込む位置を示す
+	ch           byte   // 現在検査中の文字
 }
 
+/*
+	ソースコード（input） から 字句解析器（Lexer 型の構造体）を生成.
+	readChar() で初期化.
+ */
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
 }
 
-// TODO: Bacon で Unicode と絵文字をサポートする.
+/*
+	ソースコードの次の一文字（readPosition）を読んで, 現在位置（position）を進める.
+	「ch = 0」は「まだ何も読み込んでいない」もしくは「ファイルの終わり」を表す.
+	TODO: Bacon で Unicode と絵文字をサポートする.
+ */
 func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
 		l.ch = 0
@@ -25,10 +34,17 @@ func (l *Lexer) readChar() {
 	l.readPosition += 1
 }
 
+/*
+	現在検査中の文字（ch） に一致する Bacon の Token を返す.
+	Token を返す前に, 入力のポインタを返す.
+ */
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhitespace()
+
 	switch l.ch {
+
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
 	case '+':
@@ -48,11 +64,72 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupTokenType(tok.Literal)
+			return tok
+		} else if isDisit(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 	l.readChar()
 	return tok
 }
 
+/*
+	ch が 識別子 / キーワードの一部であれば,
+	読み終えるまでポインタを進めて, 読み込んだ識別子 / キーワードの文字列を返す.
+ */
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+/*
+	ch が整数であれば, 読み終えるまでポインタを進めて, 読み込んだ整数を文字列で返す.
+ */
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDisit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+/*
+	スペースやタブ, 改行を読み飛ばすためのヘルパー関数.
+ */
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+/*
+	予期しない文字が来た時に, token.ILLEGAL トークンとして扱うためのヘルパー関数.
+ */
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+/*
+	与えられた文字が, 英字もしくは"_"か判定するヘルパー関数.
+ */
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+/*
+	与えられた文字が, 数字か判定するヘルパー関数.
+ */
+func isDisit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
--- token/token.go
+++ token/token.go
@@ -1,25 +1,62 @@
 package token
 
+/*
+	Bacon 言語におけるトークンタイプを示す型.
+ */
 type TokenType string
 
+/*
+	Bacon 言語におけるトークンを表す構造体.
+ */
 type Token struct {
 	Type    TokenType
 	Literal string
 }
 
+/*
+	Bacon 言語におけるキーワード.
+ */
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+/*
+	Token の Literal から, その Token の TokenTypeを探す関数.
+ */
+func LookupTokenType(literal string) TokenType {
+	if tok, ok := keywords[literal]; ok {
+		return tok
+	}
+	return IDENT
+}
+
+/*
+	Bacon 言語におけるトークンタイプの種類.
+ */
 const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
 
-	// Identifiers + literals
-	IDENT = "IDENT"
-	INT   = "INT"
+	// 識別子 + リテラル
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1234567
 
-	// Operators
+	// 演算子
 	ASSIGN   = "="
 	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
 
-	// Delimiters
+	LT = "<"
+	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// デリミタ
 	COMMA     = ","
 	SEMICOLON = ";"
 
@@ -28,9 +65,12 @@ const (
 	LBRACE = "{"
 	RBRACE = "}"
 
-	// Keywords
+	// キーワード
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
-
-
