57b45317e5d7e446fe7211a661523d55a94b4e26
WTBacon <wtbacon514@gmail.com>
2019-05-12 04:11:50 +0900

    呼び出し式をパースできるように追加

--- ast/ast.go
+++ ast/ast.go
@@ -306,7 +306,7 @@ func (bs *BlockStatement) String() string {
 }
 
 type FunctionLiteral struct {
-	Token      token.Token // The 'fn' token
+	Token      token.Token // 'fn' トークン
 	Parameters []*Identifier
 	Body       *BlockStatement
 }
@@ -326,6 +326,30 @@ func (fl *FunctionLiteral) String() string {
 	out.WriteString(strings.Join(params, ", "))
 	out.WriteString(") ")
 	out.WriteString(fl.Body.String())
+
+	return out.String()
+}
+
+type CallExpression struct {
+	Token     token.Token // '(' トークン
+	Function  Expression  // Identifier か FunctionLiteral
+	Arguments []Expression
+}
+
+func (ce *CallExpression) expressionNode()      {}
+func (ce *CallExpression) TokenLiteral() string { return ce.Token.Literal }
+func (ce *CallExpression) String() string {
+	var out bytes.Buffer
+
+	args := []string{}
+	for _, a := range ce.Arguments {
+		args = append(args, a.String())
+	}
+
+	out.WriteString(ce.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
 
 	return out.String()
 }
